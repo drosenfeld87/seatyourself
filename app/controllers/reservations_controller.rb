90c7c8451a84aa41101a02bce4e0f3635e4daa52
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-06 15:36:48 -0500

    Add flash notice once new reservation created

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,7 +9,20 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    @reservation = Reservation.new
 
+    @reservation.email = params[:reservation][:email]
+    @reservation.restaurant_name = params[:reservation][:restaurant_name]
+    @reservation.date = params[:reservation][:date]
+    @reservation.time = params[:reservation][:time]
+    @reservation.number_of_people = params[:reservation][:number_of_people]
+
+    if @reservation.save
+      flash[:alert] = "Congratulations! Your email confirmation is on the way."
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def show
