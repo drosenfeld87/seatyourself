b27967bf14f3f718b430e8e67b6fb78897d34aee
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-06 16:48:24 -0500

    Add simple error in case reservation time falls outside of restaurant hours

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,12 +10,19 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = Reservation.new
+    # Uncomment below once there is a Restaurant model
+    # @restaurant = Restaurant.find_by(name: params[:reservation][:restaurant_name])
 
     @reservation.email = params[:reservation][:email]
     @reservation.restaurant_name = params[:reservation][:restaurant_name]
     @reservation.date = params[:reservation][:date]
     @reservation.time = params[:reservation][:time]
     @reservation.number_of_people = params[:reservation][:number_of_people]
+
+    unless @reservation.time < 16 && @reservation.time >= 23
+      flash[:alert] = "The reservation time must fall within open hours"
+      redirect_to root_url
+    end
 
     if @reservation.save
       flash[:alert] = "Congratulations! Your email confirmation is on the way."
