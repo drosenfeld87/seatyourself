548420d2c3fa88f690f669c8428ed3e3a4ae4685
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-08 14:44:39 -0500

    Add loyalty points column to users table and made it so loyalty points increase by 10 upon successful booking of a reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308182604) do
+ActiveRecord::Schema.define(version: 20180308193629) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "email"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180308182604) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "loyalty_points"
   end
 
 end
