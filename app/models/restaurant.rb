7f49cf78efb873dd7d1703303a1e0f526635548a
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-08 12:39:04 -0500

    Add available time slots for restaurant page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,7 @@ class Restaurant < ApplicationRecord
   has_many :reservations
 
   belongs_to :user
+  # belongs_to :visited, :polymorphic => true
 
   validates :restaurant_name, :url, :hours_open, :hours_closed, :capacity, :address, :neighbourhood, :price_range, :summary, :restaurant_url, presence: true
 
@@ -12,6 +13,21 @@ class Restaurant < ApplicationRecord
   def restaurant_hours
     if hours_open >= hours_closed
       errors.add(:hours_closed, "Closing time cannot be earlier than the opening time")
+    end
+  end
+
+  def available_time_slots(date)
+    hours = (hours_open..hours_closed-1).to_a
+    hours.select do |hour|
+      number_of_people_at_this_hour = 0
+
+      reservations.each do |reservation|
+        if reservation.date == date && reservation.time == hour
+          number_of_people_at_this_hour += reservation.number_of_people
+        end
+      end
+
+      capacity - number_of_people_at_this_hour >= 2
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :reservations
   has_many :restaurants
-  # has_many :restaurants, :through => :reservations, as: :visited
+  # has_many :restaurants, :through => :reservations, :as => :visited
+  # :foreign_key => "restaurant_id", :class_name => "Restaurant"
 
   has_secure_password
 
